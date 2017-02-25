0e76cbdddfa880b6195c41d1ceac41a01ba110bc
stanchiang <stanchiang23@gmail.com>
2017-02-25 13:25:45 -0800

    added basic ui elements to scrubberview

--- PinVid/PinVid/ScrubberView.swift
+++ PinVid/PinVid/ScrubberView.swift
@@ -10,12 +10,74 @@ import UIKit
 
 class ScrubberView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    var rangeSlider: RangeSlider!
+    var addClip:UIButton!
+    var startTimeLabel:UILabel!
+    var endTimeLabel:UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        rangeSlider = RangeSlider(frame: frame)
+        rangeSlider.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(rangeSlider)
+        rangeSlider.addTarget(self, action: #selector(rangeSliderValueChanged),
+                              for: .valueChanged)
+        
+        addClip = UIButton()
+        addClip.translatesAutoresizingMaskIntoConstraints = false
+        addClip.addTarget(self, action: #selector(addClipPressed), for: .touchUpInside)
+        addClip.backgroundColor = UIColor.green
+        addClip.setTitle("Add Clip", for: .normal)
+        addSubview(addClip)
+        
+        startTimeLabel = UILabel()
+        startTimeLabel.backgroundColor = UIColor.blue
+        startTimeLabel.translatesAutoresizingMaskIntoConstraints = false
+        startTimeLabel.text = "Start: 0:00"
+        startTimeLabel.textAlignment = .center
+        addSubview(startTimeLabel)
+        
+        endTimeLabel = UILabel()
+        endTimeLabel.backgroundColor = UIColor.red
+        endTimeLabel.translatesAutoresizingMaskIntoConstraints = false
+        endTimeLabel.text = "End: 0:00"
+        endTimeLabel.textAlignment = .center
+        addSubview(endTimeLabel)
     }
-    */
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
+    override func layoutSubviews() {
+        
+        rangeSlider.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        rangeSlider.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        rangeSlider.topAnchor.constraint(equalTo: topAnchor, constant: self.frame.height / 3).isActive = true
+        rangeSlider.heightAnchor.constraint(equalToConstant: self.frame.height / 3).isActive = true
+        
+        addClip.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        addClip.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        addClip.topAnchor.constraint(equalTo: rangeSlider.bottomAnchor).isActive = true
+        addClip.heightAnchor.constraint(equalToConstant: self.frame.height / 3).isActive = true
+        
+        startTimeLabel.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        startTimeLabel.trailingAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        startTimeLabel.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        startTimeLabel.bottomAnchor.constraint(equalTo: rangeSlider.topAnchor).isActive = true
+
+        endTimeLabel.leadingAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        endTimeLabel.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        endTimeLabel.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        endTimeLabel.bottomAnchor.constraint(equalTo: rangeSlider.topAnchor).isActive = true
+    }
+    
+    func rangeSliderValueChanged(){
+        print("lowerVal = \(rangeSlider.lowerValue) | heigherVal = \(rangeSlider.upperValue)")
+    }
+    
+    func addClipPressed(sender:UIButton){
+        print("addclip")
+    }
 }
