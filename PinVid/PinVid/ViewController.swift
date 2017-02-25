813125c908336edad2debe23ace20577bb3b758b
Max Wang <max.wang.cs@gmail.com>
2017-02-25 12:49:39 -0800

    write read

--- PinVid/PinVid/ViewController.swift
+++ PinVid/PinVid/ViewController.swift
@@ -15,6 +15,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         ref = FIRDatabase.database().reference()
         self.ref.child("users/123/username").setValue("max")
+        ref.child("title").observeSingleEvent(of: .value, with: { (snapshot) in
+            if let dict = snapshot.value as? NSDictionary {
+                print(dict)
+            }
+        }) { (err) in
+            print("error: \(err)")
+        }
 
     }
 
