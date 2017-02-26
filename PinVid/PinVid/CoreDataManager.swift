3e6c6e217dff20046950f06a46f3f47ccfa5fc0e
yhu98 <yhu98@ucsc.edu>
2017-02-25 17:11:03 -0800

    Started CoreData Manager

---
+++ PinVid/PinVid/CoreDataManager.swift
@@ -0,0 +1,50 @@
+import Foundation
+import UIKit
+import CoreData
+
+class CoreDataManager {
+    
+    static let sharedInstance = CoreDataManager()
+    
+    ///Featch all video when app opens
+    func fetchVideo() -> [NSManagedObject] {
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return [NSManagedObject]()
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Video")
+        do {
+            return try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Could not fetch \(error)")
+        }
+        
+        return [NSManagedObject]()
+    }
+    
+    func addVideo()  {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let entity = NSEntityDescription.entity(forEntityName: "Video", in: managedContext)
+        
+        let video = NSManagedObject(entity: entity!, insertInto: managedContext)
+        
+        video.setValue("Jay's ID", forKey: "montage_id")
+        video.setValue("Jay's URL", forKey: "yt_url")
+        video.setValue("Jay's Title", forKey: "title")
+        video.setValue("Jay's Desc", forKey: "desc")
+        video.setValue("Jay's Author", forKey: "author")
+
+        
+        
+        
+    }
+    
+}
