bb9c3bf2170370d5c89d5ad4212b677837386bc3
John Regner <regnerjr@gmail.com>
2017-02-26 14:33:11 -0800

    Move work on Detail View

--- PinVid/PinVid/ProfieList/DetailViewController.swift
+++ PinVid/PinVid/ProfieList/DetailViewController.swift
@@ -1,33 +1,67 @@
-//
-//  DetailViewController.swift
-//  PinVid
-//
-//  Created by Sida Wang on 2/26/17.
-//  Copyright © 2017 SantaClaraiOSConnect. All rights reserved.
-//
 
 import UIKit
 
 class DetailViewController: UIViewController {
+
+    var setLoop = false
+    lazy var start:Float = { Float(self.montage.clips.first!.start_time!) }()
+    lazy var end:Float = { Float(self.montage.clips.first!.end_time!) }()
+
     var montage: Montage!
+
+    @IBOutlet var player: YTPlayerView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        player.delegate = self
+
+        montage = Montage()
+        montage.author = "Max"
+        montage.yt_url = "https://www.youtube.com/watch?v=xecEV4dSAXE"
+        montage.desc = "This is another test."
+        montage.title = "MAX!"
+        montage.montage_id = "1111AAAA"
+        montage.clips = [Clip(startTime: 10, endTime: 20, thumbNailUrl: nil)]
+
+        guard let url = montage.yt_url
+        else {
+            return
+        }
+
+        //player.loadVideo(byURL: url, startSeconds: start, endSeconds: end, suggestedQuality: .highRes)
+        //player.load(withVideoId: "nS-rnG_DGHA")
+        let playerVars = ["controls":0,"playsinline":1, "autohide":1, "showinfo":0, "modestbranding":1, "autoplay":1]
+        player.load(withVideoId: "xecEV4dSAXE", playerVars: playerVars)
+        //player.loadVideo(byId: "nS-rnG_DGHA", startSeconds: start, endSeconds: end, suggestedQuality: .small)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        player.pauseVideo()
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+extension DetailViewController: YTPlayerViewDelegate {
+    func playerViewDidBecomeReady(_ playerView: YTPlayerView) {
+
+        playerView.loadVideo(byId: "", startSeconds: start, endSeconds: end, suggestedQuality: .small)
+        playerView.playVideo()
+    }
+
+    func playerView(_ playerView: YTPlayerView, didChangeTo state: YTPlayerState) {
+        print("changed to state \(state)")
+    }
+
+    func playerView(_ playerView: YTPlayerView, didChangeTo quality: YTPlaybackQuality) {
+        print("Quality \(quality)")
+    }
+
+    func playerView(_ playerView: YTPlayerView, receivedError error: YTPlayerError) {
+        print("got Error \(error)")
+    }
+    func playerView(_ playerView: YTPlayerView, didPlayTime playTime: Float) {
+        print("Played \(playTime)")
+    }
+}
+
